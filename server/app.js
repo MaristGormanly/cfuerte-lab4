a6d65754f0aafc5dc74d5bd1199feb95106adf57
Caroline Fuerte <Caroline.fuerte1@marist.edu>
2024-05-03 05:18:53 -0400

    Commit message

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('client/public'));
 
 app.get('/', function (req, res) {
 	res.sendFile('index.html', {root: './client/views' });
 });
 
+app.get('/', function (req, res) {
+	res.sendFile('styles.css', {root: './client/public/css' });
+});
+
+app.get('/', function (req, res) {
+    res.sendFile('login.html', {root: './client/views'});
+});
+
+//Starting the server
 app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
